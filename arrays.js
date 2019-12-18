9e3072e0eb732d2e893acc2b5587225e96e0dc15
vishal8995 <vishal8995@gmail.com>
2019-12-18 23:07:39 +0530

    Day 2

---
+++ arrays.js
@@ -0,0 +1,7 @@
+//array objects and its size is dynamic
+
+let selectedColors = ['red', 'blue'];
+selectedColors[2] = 'green';
+selectedColors[3] = 4;
+console.log(selectedColors);
+console.log(selectedColors[1]);
---
+++ object.js
@@ -0,0 +1,14 @@
+let person = {
+    name: "V",
+    age: 26
+}
+
+//access propery
+
+//dot notation
+person.name = "B";
+console.log(person.name);
+
+//bracket notation
+person['name'] = 'C';
+console.log(person.name);
