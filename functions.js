0cc304beb99b0b2965e9f74d88695be21e57172e
vishal8995 <vishal8995@gmail.com>
2019-12-19 00:25:51 +0530

    Functions

---
+++ functions.js
@@ -0,0 +1,16 @@
+function greet(name, lastname){
+
+    console.log("Hello " + name + " " + lastname + " !!");
+}
+
+greet("V", "S");
+
+
+
+function square(number){
+
+    return number*number;
+}
+
+let num = square(2);
+console.log(num);
